1e207919ce5d1ec6a7db35d7ab779b79814b0396
Emperor Constantine <114262060+StenLY2022@users.noreply.github.com>
2022-09-30 22:48:01 +0300

    Create REDMY_for_C++BOOK_variant2_inMAIN.cpp
    Процессы gcc

---
+++ REDMY_for_C++BOOK_variant2_inMAIN.cpp
@@ -0,0 +1,22 @@
+C++BOOK_variant2_inMAIN.cpp
+"ConsoleApplication1.exe" (Win32). Загружено "Q:\MyC++Project_EllerProject\Упражнения по учебнику\Variant2\#1\ConsoleApplication1\Debug\ConsoleApplication1.exe". Символы загружены.
+"ConsoleApplication1.exe" (Win32). Загружено "C:\Windows\SysWOW64\ntdll.dll". 
+"ConsoleApplication1.exe" (Win32). Загружено "C:\Windows\SysWOW64\kernel32.dll". 
+"ConsoleApplication1.exe" (Win32). Загружено "C:\Windows\SysWOW64\KernelBase.dll". 
+"ConsoleApplication1.exe" (Win32). Загружено "C:\Program Files\Norton Security\NortonData\22.9.2.3\Definitions\BASHDefs\20180711.005\UMEngx86.dll". 
+"ConsoleApplication1.exe" (Win32). Загружено "C:\Windows\SysWOW64\msvcp140d.dll". 
+"ConsoleApplication1.exe" (Win32). Загружено "C:\Windows\SysWOW64\vcruntime140d.dll". 
+"ConsoleApplication1.exe" (Win32). Загружено "C:\Windows\SysWOW64\ucrtbased.dll". 
+Поток 0x2964 завершился с кодом 0 (0x0).
+"ConsoleApplication1.exe" (Win32). Загружено "C:\Windows\SysWOW64\kernel.appcore.dll". 
+"ConsoleApplication1.exe" (Win32). Загружено "C:\Windows\SysWOW64\msvcrt.dll". 
+"ConsoleApplication1.exe" (Win32). Загружено "C:\Windows\SysWOW64\rpcrt4.dll". 
+"ConsoleApplication1.exe" (Win32). Загружено "C:\Windows\SysWOW64\sspicli.dll". 
+"ConsoleApplication1.exe" (Win32). Загружено "C:\Windows\SysWOW64\cryptbase.dll". 
+"ConsoleApplication1.exe" (Win32). Загружено "C:\Windows\SysWOW64\bcryptprimitives.dll". 
+"ConsoleApplication1.exe" (Win32). Загружено "C:\Windows\SysWOW64\sechost.dll". 
+Поток 0x3fc8 завершился с кодом 0 (0x0).
+Поток 0x350c завершился с кодом 0 (0x0).
+Поток 0x2be4 завершился с кодом 0 (0x0).
+Поток 0x273c завершился с кодом 0 (0x0).
+Программа "[5924] ConsoleApplication1.exe" завершилась с кодом 0 (0x0).
